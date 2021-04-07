a10e73474ded4bcbda65897b35fd6386857444f4
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-07 10:53:47 +0530

    Update alpha.js

--- alpha.js
+++ alpha.js
@@ -1,9 +1,4 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => res.send('Discord Bot Allive!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 // PACKAGES
 
 const Discord = require('discord.js');
