759dc74f997686d99c50e545c766c02611a6b9fe
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-07 10:53:29 +0530

    Update alpha.js

--- alpha.js
+++ alpha.js
@@ -13,30 +13,11 @@ const db = require('quick.db');
 
 const path = require("path");
 
-const chalk = require("chalk");
-const moment = require("moment"); 
-var Jimp = require("jimp");
-const request = require("request");
-const axios = require("axios");
-const snekfetch = require("snekfetch");
-const fetch = require("node-fetch");
-const { CanvasSenpai } = require("canvas-senpai");
-const canva = new CanvasSenpai();
-const canvas = require("discord-canvas"),
-welcomeCanvas = new canvas.Welcome();
-var chatbot = new alexa("chatbot api");
-
 
 const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-
-const { Player } = require('discord-player');
-const player = new Player(client);
-client.player = player;
-client.emotes = require('./configs/emotes.json')
-client.filters = require('./configs/filters.json');
 
 ["aliases", "commands"].forEach(cmd => client[cmd] = new Discord.Collection());
 ["console", "command", "event"].forEach(events => require(`./handlers/${events}`)(client));
