6553f5ed6e7168fc33e1716c3f51b1376e6e506d
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-08 11:46:10 +0530

    Create avatar.js

---
+++ commands/Moderation/avatar.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const config = require('../../configs/config.json');
+const bot = new Client();
+
+module.exports = {
+    config: {
+        name: 'avatar',
+        description: '',
+        aliases: ["av"],
+        usage: '',
+        accessableby: "",
+    },
+    run: async (bot, message, args) => {
+    let target = message.mentions.members.first() || message.member
+    if(!target) return message.reply(`No User Mentioned !`)
+   const avembed = new MessageEmbed()
+   .setTitle(`AVATAR OF ${target.user.tag}`)
+   .setColor('RANDOM')
+   .setImage(target.user.displayAvatarURL({dynamic : true , size: 4096 ,format:"png"}))
+   .setFooter(`Requested by ${message.author.tag}`)
+   .setTimestamp()
+   
+    message.channel.send(avembed)
+    }
+}
+
