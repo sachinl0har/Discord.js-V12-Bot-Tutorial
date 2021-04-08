68aeecfd16a8a512ac0932d5c29acc7f7ddd589e
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-08 11:51:41 +0530

    Create serverinfo.js

---
+++ commands/Info/serverinfo.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const config = require('../../configs/config.json');
+
+
+module.exports = {
+    config: {
+        name: 'serverinfo',
+        description: '',
+        aliases: ["sinfo"],
+        usage: '',
+        accessableby: "",
+    },
+    run: async (client, message, args) => {
+    let server = message.guild;
+      let serverinfoembed = new Discord.MessageEmbed()
+
+    .setTitle(`SERVERINFO OF __${server.name}__`)
+    .setColor('RANDOM')
+    .setThumbnail(server.iconURL({dynamic:true}))
+    .addField(`Owner: `,`${server.owner}(ID:${server.ownerID})`,true)
+    .addField(`REGION: `,`${server.region}`,true)
+    .addField(`Channels`,`${server.channels.cache.size}`,true)
+    .addField(`Members`,`${server.members.cache.size}`,true)
+    .addField(`Roles`,`${server.roles.cache.size}`,true)
+    .addField(`Created At`,`${server.createdAt}`,true)
+    message.channel.send(serverinfoembed)
+    }
+}
+
