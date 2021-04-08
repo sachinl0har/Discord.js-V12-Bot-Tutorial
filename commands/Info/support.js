39a93c840a8c810b2349e6e409fd1b101eb09f55
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-08 11:58:07 +0530

    Create support.js

---
+++ commands/Info/support.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const config = require('../../configs/config.json');
+
+
+module.exports = {
+    config: {
+        name: 'support',
+        description: '',
+        aliases: [""],
+        usage: '',
+        accessableby: "",
+    },
+    run: async (client, message, args) => {
+    const embed = new Discord.MessageEmbed()
+    .setColor('RANDOM')
+    .setTitle(`SUPPORT SERVER INVITE`)
+    .setDescription(`[CLICK HERE](https://discord.gg/Xqw2akR)`)
+    message.channel.send(embed);
+    }
+}
+
