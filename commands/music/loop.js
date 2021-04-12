516dae246185ce8b879644e5d3a144a2873b0c30
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-12 11:23:07 +0530

    Update loop.js

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -1,10 +1,12 @@
 module.exports = {
-    name: 'loop',
-    aliases: ['lp', 'repeat'],
-    category: 'Music',
-    utilisation: '{prefix}loop',
-
-    execute(client, message, args) {
+    config: {
+        name: "loop",
+        aliases: [],
+        category: "music",
+        description: "To enable or disable the repeat function",
+        usage: "",
+        accessableby: ""
+    },
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
