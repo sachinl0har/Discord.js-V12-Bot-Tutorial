f82a5490b0405ca546a697983c138d97e6dede4b
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-12 11:24:38 +0530

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,10 +1,13 @@
 module.exports = {
-    name: 'play',
-    aliases: ['p'],
-    category: 'Music',
-    utilisation: '{prefix}play [name/URL]',
-
-    execute(client, message, args) {
+    config: {
+        name: "play",
+        aliases: [],
+        category: "music",
+        description: "Plays music in a voice channel",
+        usage: "<name>",
+        accessableby: ""
+    },
+    run: async (client, message, args) => {
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
