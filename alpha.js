b4f57522d193f87b20f35bc0b63e7358a61b77ce
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-12 11:12:34 +0530

    Update alpha.js

--- alpha.js
+++ alpha.js
@@ -15,11 +15,26 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
+const { Player } = require('discord-player');
+const player = new Player(client);
+client.player = player;
+client.emotes = require('./configs/emotes.json')
+client.filters = require('./configs/filters.json');
+
 ["aliases", "commands"].forEach(cmd => client[cmd] = new Discord.Collection());
 ["console", "command", "event"].forEach(events => require(`./handlers/${events}`)(client));
 
 client.categories = fs.readdirSync('./commands');
 
+fs.readdir('./player-events/', (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        const event = require(`./player-events/${file}`);
+        let eventName = file.split(".")[0];
+        console.log(`Loading player event ${eventName}`);
+        client.player.on(eventName, event.bind(null, client));
+    });
+});
 
 
 // EVENTS
