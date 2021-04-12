ba535d4adf4bba3ef2d7e888d9efb67aadd8e5c2
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-12 12:46:38 +0530

    Update pause.js

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -7,6 +7,7 @@ module.exports = {
         usage: "",
         accessableby: ""
     },
+     run: async (client, message, args) => {
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
