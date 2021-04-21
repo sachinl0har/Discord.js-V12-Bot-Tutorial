9e820d64b6ca4cacf8d19252018d52d1154398cf
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-21 08:50:12 +0530

    Update userinfo.js

--- commands/Info/userinfo.js
+++ commands/Info/userinfo.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../../configs/config.json');
-
+const { embedcolor } = require('../../configs/config.json')
 
 module.exports = {
     config: {
@@ -14,7 +14,7 @@ module.exports = {
      let target = message.guild.members.cache.get(message.mentions.users.first()) || bot.users.cache.get(args[1]) || message.author;
 if(!target) return message.reply(`No User Mentioned !`)
     let userinfoembed = new Discord.MessageEmbed()
-    .setColor('RANDOM')
+    .setColor(embedcolor)
     .setTitle(`USERINFO COMMAND !`)
     .setDescription(`:black_square_button: Bot : ${target.bot} \n :black_square_button: USER ID : ${target.id} \n :black_square_button: Tag : ${target.tag}\n :black_square_button: Joined Discord : ${target.createdAt} \n `)
     .setThumbnail(target.displayAvatarURL({dynamic:true}));
