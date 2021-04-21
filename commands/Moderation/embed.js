1953f86028cbd2fcdf88e3dbf936bc66c49fd312
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-21 08:45:13 +0530

    Update embed.js

--- commands/Moderation/embed.js
+++ commands/Moderation/embed.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const config = require('../../configs/config.json');
 const bot = new Discord.Client();
+const { embedcolor } = require('../../configs/config.json')
 
 module.exports = {
     config: {
@@ -16,7 +17,7 @@ if(!content) return message.reply(`Please Specify Something To Embed !`)
 
 const embedname = new Discord.MessageEmbed()
 .setDescription(content)
-.setColor('0x#00ffff')
+.setColor(embedcolor)
 .setFooter(`REQUESTED BY ${message.author.tag}`,`${message.author.displayAvatarURL({dynamic : true})}`)
 .setTimestamp();
 message.channel.send(embedname)
