aab34112db3b2193847f3a300968e53c88dab840
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-21 08:25:26 +0530

    Update w-filters.js

--- commands/music/w-filters.js
+++ commands/music/w-filters.js
@@ -1,37 +1,53 @@
+const Discord = require("discord.js");
+const db = require('quick.db');
+const { embedcolor, prefix } = require('../../configs/config.json')
+
 module.exports = {
     config: {
         name: "filters",
         aliases: [],
         category: "music",
-        description: "Show Filters",
+        description: "See Filters",
         usage: "",
         accessableby: ""
     },
     run: async (client, message, args) => {
-        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
+	const embednoinvoice = new Discord.MessageEmbed()
+	.setTitle('Error!')
+	.setDescription(`${client.emotes.error} - You're not in a voice channel !`)
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
+    .setColor(embedcolor)
+	.setTimestamp();
+	const embednosong = new Discord.MessageEmbed()
+	.setTitle('Error!')
+	.setDescription(`${client.emotes.error} - No songs currently playing !`)
 
-        if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No music currently playing !`);
+    .setColor(embedcolor)
+	.setTimestamp();
+    if (!message.member.voice.channel) return message.channel.send(embednoinvoice);
 
-        const filtersStatuses = [[], []];
+    if (!client.player.getQueue(message)) return message.channel.send(embednosong);
 
-        client.filters.forEach((filterName) => {
-            const array = filtersStatuses[0].length > filtersStatuses[1].length ? filtersStatuses[1] : filtersStatuses[0];
-            array.push(filterName.charAt(0).toUpperCase() + filterName.slice(1) + " : " + (client.player.getQueue(message).filters[filterName] ? client.emotes.success : client.emotes.off));
-        });
+    const disabledEmoji = client.emotes.error;
+    const enabledEmoji = client.emotes.success;
 
-        message.channel.send({
-            embed: {
-                color: 'ORANGE',
-                footer: { text: 'This bot uses a Github project made by Zerio (ZerioDev/Music-bot)' },
-                fields: [
-                    { name: 'Filters', value: filtersStatuses[0].join('\n'), inline: true },
-                    { name: '** **', value: filtersStatuses[1].join('\n'), inline: true },
-                ],
-                timestamp: new Date(),
-                description: `List of all filters enabled or disabled.\nUse \`${client.config.discord.prefix}filter\` to add a filter to a song.`,
-            },
-        });
-    },
+    const filtersStatuses = [[], []];
+
+    Object.keys(client.filters).forEach((filterName) => {
+        const array = filtersStatuses[0].length > filtersStatuses[1].length ? filtersStatuses[1] : filtersStatuses[0];
+        array.push(client.filters[filterName] + " : " + (client.player.getQueue(message).filters[filterName] ? enabledEmoji : disabledEmoji));
+    });
+
+    message.channel.send({
+        embed: {
+            fields: [
+                { name: 'Filters', value: filtersStatuses[0].join('\n'), inline: true },
+                { name: '** **', value: filtersStatuses[1].join('\n'), inline: true },
+            ],
+            timestamp: new Date(),
+            color: embedcolor,
+            description: `List of all filters enabled or disabled.\nUse \`${prefix}filter\` to add a filter to a song.`,
+        },
+    });
+    }
 };
