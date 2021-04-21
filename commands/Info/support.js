4a26bbf0c18a20713ee16e4a80c7b8b3b4bd52a6
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-21 08:49:11 +0530

    Update support.js

--- commands/Info/support.js
+++ commands/Info/support.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../../configs/config.json');
-
+const { embedcolor } = require('../../configs/config.json')
 
 module.exports = {
     config: {
@@ -12,7 +12,7 @@ module.exports = {
     },
     run: async (client, message, args) => {
     const embed = new Discord.MessageEmbed()
-    .setColor('RANDOM')
+    .setColor(embedcolor)
     .setTitle(`SUPPORT SERVER INVITE`)
     .setDescription(`[CLICK HERE](https://discord.gg/Xqw2akR)`)
     message.channel.send(embed);
