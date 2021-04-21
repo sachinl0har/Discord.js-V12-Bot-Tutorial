e84e88c8f7d91bcfc55c8afa5a2399f76d556149
Sachin Lohar <69034411+sachinl0har@users.noreply.github.com>
2021-04-21 07:34:19 +0530

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -1,94 +1,48 @@
 const Discord = require('discord.js');
 const config = require('../../configs/config.json');
-const { owners } = require("../../configs/config.json")
+
+
 
 module.exports = {
     config: {
         name: 'eval',
-        description: '',
+        description: 'Eval',
         aliases: [""],
-        usage: '',
-        accessableby: "",
+        usage: '<user>',
+        accessableby: "not you",
     },
     run: async (client, message, args) => {
-    const eembed = new Discord.MessageEmbed()
-            .setTitle('Evaluating...')
-    .setColor(`GREEN`)
-        const msg = await message.channel.send(eembed);
+     
+       let array = config.dev
   
-  const embed = new Discord.MessageEmbed()
-  .addField("Input", "\n`" + args.join(" ") + "`");
+  if(!array.includes(message.author.id.toString())) {
+    return message.channel.send("Only Developers can execute this command")
+  }
   
-  try {
-   const error = new Discord.MessageEmbed()
-   .setTitle("Provide a Code to eval!")
-   .setColor("#FF0000")
-    const code = args.join(" ");
-    if (!code) return msg.edit(error);
-    let evaled;
-    
-    // This method is to prevent someone that you trust, open the secret shit here.
-    if (code.toLowerCase().includes(`process.exit()`) || code.toLowerCase().includes(`client.token`) || code.toLowerCase().includes("config.token") || !owners.includes(message.author.id)) {
-      evaled = "Access Denied";
-    } else {
-      evaled = eval(code);
+        const content = message.content.split(" ").slice(1).join(" ");
+        const result = new Promise((resolve, reject) => resolve(eval(content)));
+        
+        return result.then((output) => {
+            if(typeof output !== "string"){
+                output = require("util").inspect(output, { depth: 0 });
+            }
+            if(output.includes(client.token)){
+                output = output.replace(message.client.token, "T0K3N");
+            }
+            message.channel.send({embed:{
+              title: "EVAL",
+              description: `\`\`\`js\n${output}\`\`\``,
+              color: 'RANDOM'
+            }});  
+        }).catch((err) => {
+            err = err.toString();
+            if(err.includes(message.client.token)){
+                err = err.replace(message.client.token, "T0K3N");
+            }
+            message.channel.send(err, {
+                code: "js"
+            });
+        });
+      
     }
-    
-    
-    let output = clean(evaled);
-    if (output.length > 1024) {
-      // If the output was more than 1024 characters, we're gonna export them into the hastebin.
-      const {body} = await post("https://hastebin.com/documents").send(output);
-      embed.addField("Output", `https://hastebin.com/${body.key}.js`).setColor(0x7289DA);
-      // Sometimes, the body.key will turn into undefined. It might be the API is under maintenance or broken.
-    } else {
-      embed.addField("output", "\n`" + output + "`").setColor(0x7289DA)
-  }
-                            
-                      
-await msg.edit(embed)
-            await msg.react('✅')
-            await msg.react('❌')
-            const filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✅') && (user.id === message.author.id);
-            msg.awaitReactions(filter, { max: 1 })
-                .then((collected) => {
-                    collected.map((emoji) => {
-                        switch (emoji._emoji.name) {
-                            case '✅':
-                                msg.reactions.removeAll();
-                                break;
-                            case '❌':
-                                msg.delete()
-                                break;
-                        }
-                    })
-
-                })                  
-    
-  } catch (error) {
-    let err = clean(error);
-    if (err.length > 1024) {
-      // Do the same like above if the error output was more than 1024 characters.
-      const {body} = await post("https://hastebin.com/documents").send(err);
-      embed.addField("output", `https://hastebin.com/${body.key}.js`).setColor("RANDOM");
-    } else {
-      embed.addField("output", "\n`" + err + "`").setColor("#FF0000");
-    }
-    
-    msg.edit(embed);
-    
-                        
-                    
-  }
 }
-}
-          
-               
-  function clean(string) {
-  if (typeof text === "string") {
-    return string.replace(/`/g, "`" + String.fromCharCode(8203))
-    .replace(/@/g, "@" + String.fromCharCode(8203))
-  } else {
-    return string;
-  }
-  }
